3063637870bcb22b306c66472b3670067e03da62
Jhoan Loaiza <hannerelectri@outlook.com>
2024-06-22 16:57:57 -0500

    editar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,9 @@
             <div class="hero min-h-screen bg-base-200 rounded-2xl">
                 <div class="hero-content text-center">
                   <div class="max-w-md">
-                    <h1 class="text-5xl font-bold">Hola {{ auth()->user()->name }}</h1>
+                    <h1 class="text-5xl font-bold">Bienvenido {{ auth()->user()->name }}</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <a href="{{ route('productos.index') }}" class="btn btn-primary">Consultar productos</a>
+                    <a href="{{ route('personals.index') }}" class="btn btn-primary">Consultar personals</a>
                   </div>
                 </div>
             </div>
